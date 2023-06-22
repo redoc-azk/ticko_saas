d06c0e9aa5d73360f92faef645a73705dfa1d40a
azk <aziz.kamagate@local.lan>
2023-06-22 03:56:27 +0000

    test to pause scanner

--- app/assets/controllers/qr_controller.js
+++ app/assets/controllers/qr_controller.js
@@ -4,46 +4,53 @@ import {Html5QrcodeScanner} from "html5-qrcode";
 
 export default class extends Controller {
     initialize() {
-
+        this.scanner = null;
     }
 
     connect() {
         try{
             // check if hasCamera
             //QrScanner.hasCamera().then(hasCamera => alert(`hasCamera: ${hasCamera}`));
-            console.log('qr_controller connected');
+            const _checkUuid = this.checkUuid;
             // if QrScanner object is defined
             const videoElem = document.getElementById('qr_scan_part');
             let html5QrcodeScanner = new Html5QrcodeScanner(
                 "qr_scan_part",
                 { fps: 10, qrbox: {width: 800, height: 800} },
                 /* verbose= */ false);
-            html5QrcodeScanner.render(this.onScanSuccess, this.onScanFailure);
+            html5QrcodeScanner.render(
+                function(decodedText, decodedResult){
+                    // stop scanning.
+                    html5QrcodeScanner.pause();
+                    // if scan code is a uuid v4 then
+                    if(_checkUuid(decodedText)){
+                    // /app/scanner/{scanCode} if status is 200 then alert success else alert error
+                        fetch('/app/scanner/' + decodedText)
+                            .then(response => {
+                                if(response.status === 200){
+                                    alert('Success');
+                                }else{
+                                    alert('Error');
+                                }
+                            })
+                            .catch(error => {
+                                alert('Error');
+                            });
+                    }
+                    html5QrcodeScanner.resume();
+                }, function(){});
         }catch (e) {
             alert(e);
         }
     }
 
-    onScanSuccess(decodedText, decodedResult) {
-        // handle the scanned code as you like, for example:
-        // /app/scanner/{scanCode} if status is 200 then alert success else alert error
-        fetch('/app/scanner/' + decodedText)
-            .then(response => {
-                if(response.status === 200){
-                    alert('Success');
-                }else{
-                    alert('Error');
-                }
-            })
-            .catch(error => {
-                alert('Error');
-            });
-    }
-
-    onScanFailure(error) {
-        // handle scan failure, usually better to ignore and keep scanning.
-        // for example:
-        console.warn(`Code scan error = ${error}`);
+    checkUuid(input) {
+        // if input is a uuid v4 then return true else return false
+        if(input.match(/^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$/)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
